493e90984c7b610978934d3854145dffb2babaf2
sylalex <sylalex22@gmail.com>
2023-06-25 20:46:45 +0300

    Django Forms

--- hh/hhapp/views.py
+++ hh/hhapp/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Vacancy
+from .forms import ContactForm
+from django.core.mail import send_mail
+
 
 # Create your views here.
+def home(request):
+    vacancys = Vacancy.objects.all()
+    return render(request, 'hhapp/index.html', context={'vacancys': vacancys})
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            firstname = form.cleaned_data['firstname']
+            email = form.cleaned_data['email']
+            text = form.cleaned_data['text']
+
+            send_mail(
+                firstname,
+                text,
+                email,
+                ["to@example.com"],
+                fail_silently=True,
+            )
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        form = ContactForm()
+    return render(request, 'hhapp/contact.html', {'form': form})
